7cea79983b22d9523c3f42e2d0ced1d9acf6e9bb
Hirthik-Mathesh-GV <210701084@rajalakshmi.edu.in>
2024-10-07 17:27:25 +0530

    Add files via upload

---
+++ Exp-10/Exp-10.1 output/Exp-10.1.R
@@ -0,0 +1,9 @@
+install.packages("ggplot2") 
+library(ggplot2) 
+ggplot(data = iris, aes(x = Sepal.Length, y = Sepal.Width, color = Species)) + 
+  geom_point(size = 3) +  
+  labs(title = "Scatter Plot of Sepal Dimensions",  
+       x = "Sepal Length (cm)",  
+       y = "Sepal Width (cm)") +  
+  theme_minimal()  
+
