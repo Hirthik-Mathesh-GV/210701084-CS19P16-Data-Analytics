41fe60a5ce894d6997bb2bc07848808a16f39f6a
Hirthik-Mathesh-GV <210701084@rajalakshmi.edu.in>
2024-10-19 12:46:08 +0530

    Delete Exp-10 directory

--- Exp-10/Exp-10.3 output/Exp-10.3.R
+++
@@ -1,8 +0,0 @@
-install.packages("ggplot2") 
-library(ggplot2)
-ggplot(data = iris, aes(x = Sepal.Length)) +
-  geom_histogram(binwidth = 0.3, fill = "orange", color = "black") +
-labs(title = "Histogram of Sepal Length",  
-     x = "Sepal Length (cm)",  
-     y = "Frequency") + 
-  theme_minimal()
--- Exp-10/Exp-10.4 output/Exp-10.4.R
+++
@@ -1,8 +0,0 @@
-install.packages("ggplot2")
-library(ggplot2)
-ggplot(data = iris, aes(x = Species, y = Sepal.Length, fill = Species)) + 
-  geom_boxplot() +
-  labs(title = "Box Plot of Sepal Length by Species",  
-       x = "Species",  
-       y = "Sepal Length (cm)") + 
-  theme_minimal()
